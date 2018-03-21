5d2c1acf5a4581f2f2cbd6b470214fe493cb07df
Rupak Ganguly <rupakg@gmail.com>
2018-03-20 22:44:36 -0400

    Add db calls to the lambda functions.

--- examples/retail-app/code/products.js
+++ examples/retail-app/code/products.js
@@ -1,49 +1,64 @@
-const products = [
-  { id: '0', name: 'Phlebotinum', price: 3.99 },
-  { id: '1', name: 'Unobtainium', price: 9.99 },
-  { id: '2', name: 'Skub', price: 2.49 }
-]
+/* eslint-disable no-console */
+
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+}
+
+const dynasty = require('dynasty')(credentials)
+
+const productsTable = dynasty.table('products')
 
 function create(evt, ctx, cb) {
-  cb(null, {
-    statusCode: 201,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: 'created'
-  })
+  // { id: '1', name: 'Carrots', description: 'Organic carrots', price: 8.29 }
+  const data = JSON.parse(evt.body)
+  productsTable.insert(data)
+    .then((resp) => {
+      cb(null, {
+        statusCode: 201,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: resp
+      })
+    })
 }
 
 function get(evt, ctx, cb) {
   const id = parseInt(evt.pathParameters.id || 0, 10)
-  if (!products[id]) {
-    cb(null, {
-      statusCode: 404,
-      body: 'Product not found.'
+  productsTable.find(id)
+    .then((product) => {
+      cb(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(product)
+      })
     })
-    return
-  }
-
-  cb(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(products[id])
-  })
+    .catch((err) => {
+      cb(null, {
+        statusCode: 404,
+        body: `Product with '${id}' not found. Error: ${err}`
+      })
+    })
 }
 
 function list(evt, ctx, cb) {
-  cb(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(products)
-  })
+  productsTable.scan()
+    .then((products) => {
+      cb(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(products)
+      })
+    })
 }
 
 module.exports = { create, get, list }
